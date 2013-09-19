fa2f6785ebcfde2f1a43ebf722f8e02b1f48f63d
Will Strinz <wstrinz@gmail.com>
2013-09-19 01:27:10 -0500

    basic module dsl, tests

--- lib/pan.rb
+++ lib/pan.rb
@@ -14,6 +14,41 @@ class Cogibara
     def self.register
       Cogibara.modules.unshift self.new
     end
+
+    def self.on(pattern,&block)
+      response_patterns << [pattern, block]
+    end
+
+    def self.response_patterns
+      @@response_patterns ||= []
+    end
+
+    def current_message
+      @current_message
+    end
+
+    # Override for custom behavior for now
+    #  should have this call some other method if defined, so
+    #  eg setting up current message still happens
+
+    def ask(message)
+      @current_message = message
+      selected = self.class.response_patterns.select{|patt| message.message[patt[0]]}
+      if selected.size > 0
+        selected = selected.first
+        if selected[0].is_a? Regexp
+          # vars =
+          # raise "#{vars}"
+          instance_exec(*message.message.scan(selected[0]).flatten, &selected[1])
+        elsif selected[0].is_a? String
+          instance_exec &selected[1]
+        else
+          raise "unknown pattern match type"
+        end
+      else
+        message
+      end
+    end
   end
 
   def self.onto
@@ -30,17 +65,71 @@ class Cogibara
 
   # eventuall this should be a wrapper for a spira class, so its
   # method list and interaction is less complicated for now SW ppl
-  class Message < Spira::Base
-    configure base_uri: 'http://cogi.strinz.me/messages/'
+  class Message
+    class RDFMsg < Spira::Base
+      configure base_uri: 'http://cogi.strinz.me/messages/'
 
-    property :message, predicate: Cogibara.onto_prop.message_string
-    property :response, predicate: Cogibara.onto_prop.response_to, type: RDF::URI
-    property :from, predicate: Cogibara.onto_prop.from_user, type: String
-    property :to, predicate: Cogibara.onto_prop.to_user, type: String
-    property :message_id, predicate: Cogibara.onto_prop.message_id
+      property :message, predicate: Cogibara.onto_prop.message_string
+      property :response, predicate: Cogibara.onto_prop.response, type: RDF::URI
+      property :from, predicate: Cogibara.onto_prop.from_user, type: String
+      property :to, predicate: Cogibara.onto_prop.to_user, type: String
+      property :message_id, predicate: Cogibara.onto_prop.message_id
+
+    end
+
+
+
+    def repo
+      Spira.repositories[@msg.class.configure[:reposity_name] || :default]
+    end
+
+    def response_to
+      g = rdf_msg
+      solutions = RDF::Query.execute(repo) do
+        pattern [:original, Cogibara.onto_prop.response, g.subject]
+      end
+
+      if solutions.size == 0
+        nil
+      else
+        Message.for(solutions.first[:original])
+      end
+    end
+
+    def self.for(uri)
+      self.new(uri)
+    end
+
+    def temp
+      @temp ||= {}
+    end
+
+    def initialize(uri)
+      @msg = RDFMsg.for(uri)
+    end
+
+    def rdf_msg
+      @msg
+    end
 
     def to_s
       self.subject.to_s
+    end
+
+    def response
+      Message.for(rdf_msg.response)
+    end
+
+    def method_missing(meth, *args, &block)
+      if @msg.respond_to? meth
+        if args.size >0
+          @msg.send meth, *args, &block
+        else
+          @msg.send meth, &block
+        end
+      else
+        super(meth, *args, &block)
+      end
     end
   end
 
@@ -93,6 +182,7 @@ class Cogibara
 
     def new_id
       #Maybe use uuid gem eventually?
+      # puts "new"
       "#{Time.now.nsec}_#{rand(1000)}"
     end
   end
@@ -132,11 +222,7 @@ class Cogibara
       response = mod.ask(message)
       if response.is_a? String
         response = memory.new_message(response)
-        message.response = response
-        response.from = "cogibara"
-        response.to = message.from
-        message.save
-        response.save
+        response_details(message, response)
         return response
       elsif response.is_a? Symbol
         raise "received code #{response} from #{mod}"
@@ -144,6 +230,14 @@ class Cogibara
         puts "pass along messages or return new ones"
       end
     end
+  end
+
+  def response_details(message, response)
+    message.response = response
+    response.from = "cogibara"
+    response.to = message.from
+    message.save
+    response.save
   end
 end
 
@@ -158,6 +252,7 @@ class Chatbot < Cogibara::Module
 end
 
 
+
 # Interface to XMPP communications
 # - Eventually have this handle the different
 #     protocols, communications methods, etc.
@@ -168,8 +263,8 @@ end
 class Cogi
 
   def self.ask(msg,opts={})
-    # msg =
     Cogibara.ask Cogibara.memory.new_message(msg, opts)
+
   end
 
   def self.ask_xmpp(msg)
--- spec/pan_spec.rb
+++ spec/pan_spec.rb
@@ -18,6 +18,10 @@ describe Cogibara do
       msg = Cogi.ask('hello?',from: "wstrinz@gmail.com")
       msg.to.should == "wstrinz@gmail.com"
       msg.from.should == "cogibara"
+      original = msg.response_to
+      original.response.subject.should == msg.subject
+      # resp.is_a?(Cogibara::Message).should be true
+      # resp.response_to.should == msg
     end
   end
 
@@ -38,16 +42,61 @@ describe Cogibara do
   end
 
   describe "Adding new modules" do
-    class Reverser < Cogibara::Module
-      def ask(msg)
-        msg.message.reverse
+
+    context "overriding ask method" do
+      class Reverser < Cogibara::Module
+        def ask(msg)
+          msg.message.reverse
+        end
+      end
+
+      before do
+        Reverser.register
+      end
+
+      it { Cogi.ask_local('hello?').should ==  "?olleh" }
+    end
+
+    context "using the dsl" do
+      describe "use on keyword and strings or regexps to define behavior" do
+        class DiceRoller < Cogibara::Module
+          on 'hello you' do
+            "hai dere"
+          end
+
+          on %r{^I'm (.+)} do |name|
+            "hi #{name}"
+          end
+
+          on(/roll me (\d+)d(\d+)/) do |number,size|
+            number.to_i.times.map{|t| rand(size.to_i)+1 }.join("\n")
+          end
+        end
+
+        before do
+          DiceRoller.register
+        end
+
+        it { Cogi.ask_local('hello you').should ==  "hai dere" }
+        it { Cogi.ask_local("I'm bill").should ==  "hi bill" }
+        it { Cogi.ask_local("roll me 1d150").to_i.should > 0  }
+        it { Cogi.ask_local("roll me 4d20").split("\n").size.should == 4 }
+      end
+
+      describe "can still access the raw message" do
+        class CreepyGreeter < Cogibara::Module
+          on(/.*/) do
+            "hehe... hello #{current_message.from}"
+          end
+        end
+
+        before do
+          CreepyGreeter.register
+        end
+
+        it { Cogi.ask_local('hellow').should ==  "hehe... hello local" }
       end
     end
 
-    before do
-      Reverser.register
-    end
-
-    it { Cogi.ask_local('hello?').should ==  "?olleh" }
   end
 end
